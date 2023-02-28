c75a19c86a9f4fe9c519fb26f8810a8fa3c5f42f
Lzj111 <1286177743@qq.com>
2023-02-28 17:28:59 +0800

    优化

--- src/main/java/com/lzmispt/www/audiotext/AudioTextApplication.java
+++ src/main/java/com/lzmispt/www/audiotext/AudioTextApplication.java
@@ -17,14 +17,24 @@ public class AudioTextApplication {
         SpringApplicationBuilder builder = new SpringApplicationBuilder(AudioTextApplication.class);
         builder.headless(false).web(WebApplicationType.SERVLET).run(args);
 
+        Boolean isContinue = true;
+        while (isContinue) {
+            isContinue = exec();
+        }
+
+        // 执行完成后退出程序
+        System.exit(0);
+    }
+
+    public static Boolean exec() {
         // 1> 执行
         System.out.println("****************MP3转文字******************");
-        System.out.println("******* 请输入编码：0[百度]，1[腾讯]，2[Vosk开源]，3[讯飞]，4[阿里] ");
+        System.out.println("******* 请输入编码：0[百度]，1[腾讯]，2[Vosk开源]，3[讯飞]，4[阿里] 其他退出... ");
         Scanner sca = new Scanner(System.in);
         int type = sca.nextInt();
-        String beanName = "convertAudioBaiduImpl";
+        String beanName = null;
         switch (type) {
-            case 1: // 腾讯
+            case 1: //
                 beanName = "convertAudioTencentImpl";
                 break;
             case 0: // 百度
@@ -39,11 +49,16 @@ public class AudioTextApplication {
             case 4: // 阿里
                 beanName = "convertAudioAliImpl";
                 break;
+            default:
+                break;
         }
 
+        if (null == beanName) {
+            return false;
+        }
         IConvertAudio convertAudio = BeanUtil.getInstanceByBeanName(beanName);
         if (null == convertAudio) {
-            return;
+            return false;
         }
         AudioMode audioMode = Tools.getAudioMode();
         // 记录开始时间
@@ -54,9 +69,6 @@ public class AudioTextApplication {
         Tools.soutError("=======================================");
         Tools.soutInfo("语音转写内容=>" + text);
         Tools.reckonTime(start, end);
-
-        // 执行完成后退出程序
-        System.exit(0);
+        return true;
     }
-
 }
